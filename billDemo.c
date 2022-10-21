cfb35fa54cd528613500e1e1fc262361160cb448
Minh Tran <mdtran@cpp.edu>
2022-10-20 23:48:25 -0700

    retrieve main arguments for tax and tip percentages

--- billDemo.c
+++ billDemo.c
@@ -1,31 +1,54 @@
-// int main taking in two arguments, the tax and the tip
+#include <string.h> 
+#include <stdlib.h> 
+#include "meal.h" 
+
 /*
 
-example output:
--------------------------------------------
-[Meal]: $0.00
-Tax: [user's input]% of Meal
-Tip: [user's input]% of Meal
-
-Total: 
--------------------------------------------
-end of output
+gcc billDemo.c mealTable.c
 
 */
-
 
 // code main here
+int main(int argc, char *argv[]){ 
 
-// randomly choose from Meal table for meal
+    // int main taking in two arguments, the tax and the tip
+        /*
+        example output:
+        -------------------------------------------
+        [Meal]: $0.00
+        Tax: [user's input]% of Meal
+        Tip: [user's input]% of Meal
 
-// handle arguments to get tax and tip
+        Total: 
+        -------------------------------------------
+        end of output
+        */
 
-// display all necessary info (refer to example output)
+    char *ptrTax;
+    char *ptrTip;
+    int tax;
+    int tip;
 
-// a data table of type Meal ~ another c file
-/*
-Salad: $9.95
-Soup: $4.55
-Sandwich: $13.25
-Pizza: $22.35
-*/
+    // handle arguments to get tax and tip
+    long taxArg = strtol(argv[1], &ptrTax, 10);
+    long tipArg = strtol(argv[2], &ptrTip, 10);
+    tax = (int) taxArg;
+    tip = (int) tipArg;
+
+    printf("-------------------------------------------\n");
+    for(int i=0; i < argc; i++)
+    {
+        printf("Argv [%d]: %s\n", i, argv[i]);
+    }
+    printf("-------------------------------------------\n");
+    printf("TAX: %d PERCENT\n", tax);
+    printf("TIP: %d PERCENT\n", tip);
+    printf("-------------------------------------------\n");
+
+    // randomly choose from Meal table for meal
+
+    // display all necessary info (refer to example output)
+
+
+    return EXIT_SUCCESS; 
+} 
