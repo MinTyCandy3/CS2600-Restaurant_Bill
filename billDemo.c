fe66aebc73a185cd754a7931565b7471d043bb91
Minh Tran <mdtran@cpp.edu>
2022-10-20 22:36:10 -0700

    add initial file, provided outline

---
+++ billDemo.c
@@ -0,0 +1,35 @@
+// int main taking in two arguments, the tax and the tip
+/*
+
+example output:
+-------------------------------------------
+[Meal]: $0.00
+Tax: [user's input]% of Meal
+Tip: [user's input]% of Meal
+
+Total: 
+-------------------------------------------
+end of output
+
+*/
+
+
+// code main here
+
+// randomly choose from Meal table for meal
+
+// handle arguments to get tax and tip
+
+// display all necessary info (refer to example output)
+
+
+
+// type Meal should contain: name and price ~ header file
+
+// a data table of type Meal ~ another c file
+/*
+Salad: $9.95
+Soup: $4.55
+Sandwich: $13.25
+Pizza: $22.35
+*/
